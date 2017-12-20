4cc18019a18a871629fbc6f45f75463ec1d174fa
ProgrammerAccount <tymek.janiak@wp.pl>
2017-12-20 14:35:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,21 @@
+class Bot extends Object{
+constructor(var SafeRange)
+  {
+    this.SafeRange=SafeRange;
+  }
 
+ AnalizePosition(var positionsOfEnemys)
+  {
+    for(var i=0;positionofEnemy.lenght>i;i++)
+    {
+      if(super.position/20-positionsOfEnemys[i].position/20>-5)
+        return super.position+20;
+      if(super.position/20-positionsOfEnemys[i].position/20<5)
+        return super.position-20;
+      if(super.position-positionsOfEnemys[i].position<5) 
+        return super.position+1;
+      if(super.position-positionsOfEnemys[i].position>-5)
+        return super.position-1;
+    }
+  }
+}
